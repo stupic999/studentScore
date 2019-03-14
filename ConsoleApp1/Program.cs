e424e7b8c9aeb246f7a62f1d589e271cf3f0591a
stupid999 <aierzhiguang2@gmail.com>
2019-03-14 17:11:37 +0800

    寫一C#程式，宣告一個Student類別，類別中有兩個private屬性，分別是_name和_score代表名字和分數，並分別使用存取子Name和Score來設定和取得私有屬性的值，其中_score的範圍限制在0~100，如果低於0分設為0分，若大於100則設為100分。 在主程式中建立三個Student類別物件，請使用者輸入三個學生的名字及分數，輸入時其中一人輸入負分，其中一人輸入超過100分，以驗證限制是否有效。子點數。 將cs檔、exe檔及執行結果的截圖檔(png或jpg檔)上載至elearn。 大二游戏设计程式作业

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public string StuID { get; set; }
+        public int StuScore { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            string[] stuID = new string[3];
+            int[] stuScore = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Please Type in Student Name : ");
+                student.StuID = Console.ReadLine();
+                stuID[i] = student.StuID;
+                Console.Write("Please Type in Student Score : ");
+                int score = int.Parse(Console.ReadLine());
+                if (score > 100)
+                {
+                    score = 100;
+                }
+                else if (score < 0)
+                {
+                    score = 0;
+                }
+                student.StuScore = score;
+                stuScore[i] = student.StuScore;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Student Name : " + stuID[i]);
+                Console.WriteLine("Student Score:" + stuScore[i]);
+                Console.ReadLine();
+            }
+            Console.Write("Press Any Button To Quit");
+            Console.ReadKey();
+        }
+    }
+}
